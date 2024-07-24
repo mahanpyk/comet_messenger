15e2a61387d06adbeab7fad66731a220f072d9d1
MahanPYK <m.pourYazdankhah@middleeastbank.ir>
2024-07-24 20:05:35 +0330

    - add sample chat response

--- lib/app/core/app_constants.dart
+++ lib/app/core/app_constants.dart
@@ -3,12 +3,17 @@ class AppConstants {
   static const String TOKEN = 'TOKEN';
   static const String REFRESH_TOKEN = 'REFRESH_TOKEN';
   static const String LANGUAGE = 'LANGUAGE';
-  static const String BASE_URL = 'https://api.mainnet-beta.solana.com';
+
+  // static const String BASE_URL = 'https://api.mainnet-beta.solana.com';
+  static const String BASE_URL = 'https://api.devnet.solana.com';
   static const String WALLETS = 'WALLETS';
   static const String MNEMONIC = 'MNEMONIC';
   static const String PIN = 'PIN';
   static const String PROFILE_PROGRAM_ID = "5nFHkukP8jGuQEzpMH1H6hk5i6HUt6KamZw2NuSr97Ru";
-  static const String CONTACT_PDA = "D4VSz2XZviYv2fH4eR4W12Dv4b1XKRqM5XMaDPvKRALo";
+
+  // static const String CONTACT_PDA = "D4VSz2XZviYv2fH4eR4W12Dv4b1XKRqM5XMaDPvKRALo";
+  static const String CONTACT_PDA =
+      "J98ZB5pyfqah9xxue6HBYkj2xxsyga43vt3fG772iLoH";
   static const String BASE58_CODEC = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz';
   static const String PRIVATE_KEY =
       'MIICdQIBADANBgkqhkiG9w0BAQEFAASCAl8wggJbAgEAAoGBAKAUZV+tjiNBKhlBZbKBnzeugpdYPhh5PbHanjV0aQ+LF7vetPYhbTiCVqA3a+Chmge44+prlqd3qQCYra6OYIe7oPVq4mETa1c/7IuSlKJgxC5wMqYKxYydb1eULkrs5IvvtNddx+9O/JlyM5sTPosgFHOzr4WqkVtQ71IkR+HrAgMBAAECgYAkQLo8kteP0GAyXAcmCAkA2Tql/8wASuTX9ITD4lsws/VqDKO64hMUKyBnJGX/91kkypCDNF5oCsdxZSJgV8owViYWZPnbvEcNqLtqgs7nj1UHuX9S5yYIPGN/mHL6OJJ7sosOd6rqdpg6JRRkAKUV+tmN/7Gh0+GFXM+ug6mgwQJBAO9/+CWpCAVoGxCA+YsTMb82fTOmGYMkZOAfQsvIV2v6DC8eJrSa+c0yCOTa3tirlCkhBfB08f8U2iEPS+Gu3bECQQCrG7O0gYmFL2RX1O+37ovyyHTbst4s4xbLW4jLzbSoimL235lCdIC+fllEEP96wPAiqo6dzmdH8KsGmVozsVRbAkB0ME8AZjp/9Pt8TDXD5LHzo8mlruUdnCBcIo5TMoRG2+3hRe1dHPonNCjgbdZCoyqjsWOiPfnQ2Brigvs7J4xhAkBGRiZUKC92x7QKbqXVgN9xYuq7oIanIM0nz/wq190uq0dh5Qtow7hshC/dSK3kmIEHe8z++tpoLWvQVgM538apAkBoSNfaTkDZhFavuiVl6L8cWCoDcJBItip8wKQhXwHp0O3HLg10OEd14M58ooNfpgt+8D8/8/2OOFaR0HzA+2Dm';
